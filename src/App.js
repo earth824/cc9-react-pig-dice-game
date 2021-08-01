b449e96d1fca465341ffd4d67981bf01edd3f790
pisutthee <pisut.flyinggiraffe@gmail.com>
2021-08-02 00:11:44 +0700

    finish css

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header className="header">
+        <button className="btn">New Game</button>
       </header>
-    </div>
+      <div className="container">
+        <div className="card active">
+          <h1>Player 1</h1>
+          <p className="player-score">0</p>
+        </div>
+        <div className="card">
+          <div className="row">
+            <div className="btn btn-danger btn-fw">Roll</div>
+            <div className="btn btn-success btn-fw">Keep</div>
+          </div>
+          <div className="row">
+            <div className="dice-score">0</div>
+          </div>
+          <div className="row">
+            <p className="round-score">Round Score: 0</p>
+          </div>
+        </div>
+        <div className="card">
+          <h1>Player 2</h1>
+          <p className="player-score">0</p>
+        </div>
+      </div>
+    </>
   );
 }
 
